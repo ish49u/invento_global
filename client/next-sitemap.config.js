b48ddf3d535bf01852c2a4141669f311f5d1ff8a
ish49u <ishuishi49@gmail.com>
2025-06-08 20:50:00 +0530

    final changes 2

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,5 +1,18 @@
-/** @type {import('next-sitemap').IConfig} */
+// next-sitemap.config.js
+
 module.exports = {
   siteUrl: 'https://inventoglobal.com',
   generateRobotsTxt: true,
+  sitemapSize: 5000,
+  outDir: './public',
+
+  additionalPaths: async (config) => {
+    return [
+      await config.transform(config, '/'),
+      await config.transform(config, '/about'),
+      await config.transform(config, '/contact'),
+      await config.transform(config, '/founder'),
+      await config.transform(config, '/products'),
+    ];
+  },
 };
