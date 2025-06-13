1d8f211c61a979fc9a58456081118fd0ff63b9bd
ish49u <ishuishi49@gmail.com>
2025-06-13 17:30:42 +0530

    Upd

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Hero from "./(pages)/(home)/hero/page";
 import Products from "./(pages)/(home)/products-main/page";
 import GlobalReach from "./(pages)/(home)/globalReach/page";
@@ -7,7 +8,26 @@ import Testimonials from "./(pages)/(home)/testimonials/page";
 import Certificates from "./(pages)/(home)/certificates/page";
 import WhyChooseUs from "./(pages)/(home)/whyChooseUs/page";
 
-function page() {
+function Page() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "application/ld+json";
+    script.innerHTML = JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      name: "Invento Global",
+      url: "https://inventoglobal.com",
+      description:
+        "Merchant Exporters, Manufacturers and Suppliers of Hardware, Hand Tools, Fasteners, Screws, Copper Handicrafts and more.",
+      logo: "https://inventoglobal.com/logo.png", // Update to actual logo URL
+      sameAs: [
+        "https://www.linkedin.com/company/invento-global",
+        "https://www.facebook.com/inventoglobal",
+      ],
+    });
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <div>
       <Hero />
@@ -21,4 +41,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
